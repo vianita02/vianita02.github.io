5e42f17d62020487715d43a14422154f2774dfe1
vianita02 <vianita@byu.edu>
2019-02-23 19:15:06 -0700

    gallery finished

---
+++ assignments/lesson-7/scripts/windchill.js
@@ -0,0 +1,13 @@
+
+    var temperature = 50;
+    var windSpeed = 10;
+    var windChill = (35.74 + 0.6215 * temperature )- (35.75 * Math.pow(windSpeed,0.16)) + ( 0.4275* temperature*  Math.pow(windSpeed,0.16));
+
+    document.getElementById('windChill').innerHTML= "Wind Chill: " + windChill.toFixed (0) + "&deg;";
+    document.getElementById('windSpeed').innerHTML= windSpeed;
+    document.getElementById('temperature').innerHTML= temperature;
+    
+
+    
+       
+    
